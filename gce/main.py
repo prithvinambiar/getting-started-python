76b99a40d1a409ca6563cec3b37dae8827384863
Prithvi Nambiar <prithvin@prithvin-macbookpro.roam.corp.google.com>
2020-01-26 13:07:53 +0530

    Adding cache control public

--- gce/main.py
+++ gce/main.py
@@ -199,6 +199,7 @@ def analyse(request, is_sentiment):
   # Set CORS headers for the main request
   headers = {
     'Access-Control-Allow-Origin': '*',
+    'Cache-Control': 'public',
     'Cache-Control': 'max-age=1036808000' # 3 months
   }
   
